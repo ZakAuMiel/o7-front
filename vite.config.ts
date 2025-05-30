a6dc26d9b6b8d49f7ff8494111aacf4983bd25ca
ZakAuMiel <you@example.com>
2025-05-30 23:24:01 +0200

    don't know what i'm doing

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,25 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { resolve } from 'path'
+import { copyFileSync } from 'fs'
 
 export default defineConfig({
   plugins: [vue()],
-  base: '/', // optionnel si tu déploies à la racine
+  base: '/',
+  build: {
+    rollupOptions: {
+      // petite astuce vite : hook post-build manuel
+      plugins: [
+        {
+          name: 'copy-redirects',
+          writeBundle() {
+            copyFileSync(
+              resolve(__dirname, 'public/redirects'),
+              resolve(__dirname, 'dist/_redirects')
+            )
+          }
+        }
+      ]
+    }
+  }
 })
